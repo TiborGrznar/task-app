bf19d162d0fea461a701c2417375202011f909bd
tgrznar <tgrznar@student.umb.sk>
2026-07-12 18:25:24 +0200

    Add Task entity

---
+++ src/main/java/sk/tiborgrznar/task_app/entity/Task.java
@@ -0,0 +1,34 @@
+package sk.tiborgrznar.task_app.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private String text;
+
+    private LocalDateTime createdAt;
+
+    private boolean done;
+
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
