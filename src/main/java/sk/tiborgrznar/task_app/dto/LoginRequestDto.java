fd700534f2a9ce9fd01871549f612c7dd7521de5
tgrznar <tgrznar@student.umb.sk>
2026-07-13 13:48:44 +0200

    Add LoginRequestDto

---
+++ src/main/java/sk/tiborgrznar/task_app/dto/LoginRequestDto.java
@@ -0,0 +1,19 @@
+package sk.tiborgrznar.task_app.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequestDto {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+}
