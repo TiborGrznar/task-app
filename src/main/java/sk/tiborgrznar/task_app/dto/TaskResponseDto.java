18d1500a13b1da0c71f4c1f9913e63a4a8082f10
tgrznar <tgrznar@student.umb.sk>
2026-07-13 14:06:51 +0200

    Edit TaskResponseDto

--- src/main/java/sk/tiborgrznar/task_app/dto/TaskResponseDto.java
+++ src/main/java/sk/tiborgrznar/task_app/dto/TaskResponseDto.java
@@ -2,6 +2,7 @@ package sk.tiborgrznar.task_app.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import sk.tiborgrznar.task_app.entity.Task;
 
 import java.time.LocalDateTime;
 
@@ -16,4 +17,11 @@ public class TaskResponseDto {
     private boolean done;
 
     private LocalDateTime createdAt;
+
+    public TaskResponseDto(Task task) {
+        this.id = task.getId();
+        this.text = task.getText();
+        this.done = task.isDone();
+        this.createdAt = task.getCreatedAt();
+    }
 }
