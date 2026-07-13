e8f34fefb6734df7127c35102c1cc30ed1723db3
tgrznar <tgrznar@student.umb.sk>
2026-07-13 13:43:20 +0200

    Add RegisterRequestDto

---
+++ src/main/java/sk/tiborgrznar/task_app/dto/RegisterRequestDto.java
@@ -0,0 +1,23 @@
+package sk.tiborgrznar.task_app.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRequestDto {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+
+    @NotBlank
+    private String name;
+}
