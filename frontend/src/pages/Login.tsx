357c3ee27f14454586269924366629d083170dbc
tgrznar <tgrznar@student.umb.sk>
2026-07-21 18:38:55 +0200

    Add working login page with real form and API call

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,38 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/useAuth";
+
+
+
+
 function Login() {
-    return <div>Login page</div>;
+    const navigate = useNavigate();
+    const { login } = useAuth();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    
+    const handleSubmit = async (event: React.SyntheticEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        await login(email, password);
+        navigate("/tasks");
+    };
+    
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="email"
+                value={email}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)} 
+            />
+            <input 
+                type="password" 
+                value={password}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)}
+            />
+            <button type="submit">Login</button>
+        </form>
+    );
 }
 
 export default Login;
